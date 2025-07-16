a2b93c4931b1686afb55efcd608aa5e32511cb28
강성구 <sungku2757@gmail.com>
2025-07-16 14:07:11 +0900

    feat: Custom Hooks를 사용하여 이미지 갤러리 만들기 answer

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,20 +1,31 @@
 import { useEffect, useState } from "react";
 
-// 데이터 fetching Custom Hook 작성하기
 const useFetch = (url) => {
-  // 1. data, loading, error 값에 대한 초기 값을 지정하세요.
-  const [data, setData] = useState();
-  const [loading, setLoading] = useState();
-  const [error, setError] = useState();
+  const [data, setData] = useState(null); // 데이터 저장용 상태
+  const [loading, setLoading] = useState(true); // 로딩 상태
+  const [error, setError] = useState(null); // 에러 상태
+
+  // API 호출 함수
   const fetchData = async () => {
-    // 2. 로딩 상태를 true로 변경합니다. ( 데이터 패칭 전 로딩 적용 )
-    // 3. fetch 를 사용하여 인자로 받은 url에서 데이터를 받아온 후 data 상태를 업데이트 해주세요.
-    // (+ 데이터를 받아온 후 상태를 업데이트 했다면, 상태를 다시 false로 변경해야 합니다.)
-    // (+ catch문을 사용하여 error 상태값을 사용하여 에러 핸들링 코드도 함께 작성해주세요.)
+    try {
+      // 기본 실행
+      setLoading(true); // 시작할 때 로딩상태
+      const response = await fetch(url); // API를 통해 데이터 호출
+      const data = await response.json(); // 받아온 내용을 JSON 형태로 변경
+      setData(data); // 데이터 상태 저장
+    } catch (error) {
+      // reject시 실행되는 내용
+      setError(error); // 에러 발생 시, 에러 상태에 저장
+    } finally {
+      // reject, fulfill 상관 없이 실행되는 내용
+      setLoading(false); // 마지막으로 로딩 상태 해제
+    }
   };
-  // 4. useEffect를 사용하여 url이 변경되는 경우에만 fetchData 함수가 실행되도록 작성하세요.
-  useEffect(() => {}, []);
-  // 5. return문을 사용하여 다른 컴포넌트에서 접근할 수 있도록 총 3가지의 상태(data, error, loading) 값과 fetchData 함수를 리턴하세요.
-  return {};
+  // useEffect로 페이지 시작시 호출, url이 변경할 때마다 재실행
+  useEffect(() => {
+    fetchData();
+  }, [url]);
+
+  return { data, loading, error, fetchData };
 };
 export default useFetch;
